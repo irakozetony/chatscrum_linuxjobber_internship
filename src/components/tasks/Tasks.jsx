97db2132bb166cc076622ebe1a32d6523b95f450
Txny <irakozeyves9@gmail.com>
2022-09-17 13:30:33 +0200

    implementing the drag and drop functionalities

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,28 +1,71 @@
 import userTasks from "../static/tasks";
-
-import React from "react";
+import "./tasks.css";
+import React, {useState} from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export default function Tasks() {
+    const [taskRoll, updateTaskRoll] = useState(userTasks)
+    const handleOnDragEnd = result =>{
+        if (!result.destination) return;
+        const items = Array.from(taskRoll)
+
+        const[reorderedItems] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItems);
+
+        updateTaskRoll(items)
+    }
     return (
         <div className="task_component">
-            <div className="task_cards">
-                <div className="card">
-                    <h3 className="card_title">Weekly tasks</h3>
-                    <div className="tasks">
-                        {userTasks.map(({id, item}, index) =>{
-                            <p className="task" key={id}>
-                                {item}
-                            </p>
-                        })}
-                    </div>
+            <DragDropContext onDragEnd={handleOnDragEnd}>
+                <div className="task_cards">
+                    <Droppable droppableId="weekly">
+                        {(provided) => (
+                            <div
+                                className="card"
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                <h3 className="card_title">Weekly tasks</h3>
+                                <div>
+                                    {taskRoll.map(({id,item }, index) => {
+                                        return (
+                                            <Draggable
+                                                key={id}
+                                                draggableId={id}
+                                                index={index}
+                                            >
+                                                {(provided) => (
+                                                    <p
+                                                        className="task"
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                        ref={provided.innerRef}
+                                                    >
+                                                        {item}
+                                                    </p>
+                                                )}
+                                            </Draggable>
+                                        );
+                                    })}
+                                </div>
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                    <Droppable droppableId="daily">
+                        {(provided) => (
+                            <div
+                                className="card"
+                                {...provided.droppableProps}
+                                ref={provided.innerRef}
+                            >
+                                <h3 className="card_title">Daily target</h3>
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
                 </div>
-                <div className="card">
-                    <h3 className="card_title">Daily target</h3>
-                    <div className="tasks">
-
-                    </div>
-                </div>
-            </div>
+            </DragDropContext>
         </div>
     );
 }
