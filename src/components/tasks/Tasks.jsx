af3b6d9c8ea73dfd8217a263c2ce2964c9242f7e
Txny <irakozeyves9@gmail.com>
2022-09-17 18:52:26 +0200

    failing to drop in dnd feature

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,19 +1,58 @@
 import userTasks from "../static/tasks";
 import "./tasks.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export default function Tasks() {
-    const [taskRoll, updateTaskRoll] = useState(userTasks)
-    const handleOnDragEnd = result =>{
+    const [weeklyTaskRoll, updateWeeklyTaskRoll] = useState(userTasks);
+    const [dailyTaskRoll, updateDailyTaskRoll] = useState(userTasks)
+    const deleteTask = (list, index) =>(
+        list.splice(index, 1)
+    )
+    const handleOnDragEnd = (result) => {
+        console.log(result);
         if (!result.destination) return;
-        const items = Array.from(taskRoll)
+        const items = Array.from(weeklyTaskRoll)
 
         const[reorderedItems] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItems);
 
-        updateTaskRoll(items)
-    }
+        updateWeeklyTaskRoll(items)
+
+        // const { source, destination } = result;
+        // if (!destination) return;
+        // if(source.droppableId === destination.droppableId){
+        //     if(source.droppableId === "weekly"){
+        //         let templist = weeklyTaskRoll
+        //         const removed = deleteTask(templist, source.index)
+        //         templist.splice(destination.index, 0, removed)
+        //         updateWeeklyTaskRoll(templist)
+        //     }
+        //     else{
+        //         let templist = dailyTaskRoll
+        //         const removed = deleteTask(templist, source.index)
+        //         templist.splice(destination.index, 0, removed)
+        //         updateDailyTaskRoll(templist)
+        //     }
+        // }
+        // else{
+        //     let tempWeeklyList = weeklyTaskRoll
+        //     let tempDailyList = dailyTaskRoll
+
+        //     if(source.droppableId === "weekly"){
+        //         const removed = deleteTask(tempWeeklyList, source.index)
+        //         tempDailyList.splice(destination.index, 0, removed)
+        //         updateWeeklyTaskRoll(tempWeeklyList)
+        //         updateDailyTaskRoll(tempDailyList)
+        //     }
+        //     else{
+        //         const removed = deleteTask(tempDailyList, source.index)
+        //         tempWeeklyList.splice(destination.index, 0, removed)
+        //         updateWeeklyTaskRoll(tempWeeklyList)
+        //         updateDailyTaskRoll(tempDailyList)
+        //     }
+        // }
+    };
     return (
         <div className="task_component">
             <DragDropContext onDragEnd={handleOnDragEnd}>
@@ -27,26 +66,30 @@ export default function Tasks() {
                             >
                                 <h3 className="card_title">Weekly tasks</h3>
                                 <div>
-                                    {taskRoll.map(({id,item }, index) => {
-                                        return (
-                                            <Draggable
-                                                key={id}
-                                                draggableId={id}
-                                                index={index}
-                                            >
-                                                {(provided) => (
-                                                    <p
-                                                        className="task"
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                        ref={provided.innerRef}
-                                                    >
-                                                        {item}
-                                                    </p>
-                                                )}
-                                            </Draggable>
-                                        );
-                                    })}
+                                    {weeklyTaskRoll.map(
+                                        ({ id, item }, index) => {
+                                            return (
+                                                <Draggable
+                                                    key={id}
+                                                    draggableId={id}
+                                                    index={index}
+                                                >
+                                                    {(provided) => (
+                                                        <p
+                                                            className="task"
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            ref={
+                                                                provided.innerRef
+                                                            }
+                                                        >
+                                                            {item}
+                                                        </p>
+                                                    )}
+                                                </Draggable>
+                                            );
+                                        }
+                                    )}
                                 </div>
                                 {provided.placeholder}
                             </div>
@@ -60,7 +103,9 @@ export default function Tasks() {
                                 ref={provided.innerRef}
                             >
                                 <h3 className="card_title">Daily target</h3>
-                                {provided.placeholder}
+                                <div>
+                                    {provided.placeholder}
+                                </div>
                             </div>
                         )}
                     </Droppable>
